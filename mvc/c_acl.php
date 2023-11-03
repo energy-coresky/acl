d7c1be407d5a7a9776140d602de287c95c050e6a
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-11-03 11:29:06 +0300

    Next

--- mvc/c_acl.php
+++ mvc/c_acl.php
@@ -2,7 +2,12 @@
 
 class c_acl extends Controller
 {
-    function a_profile() {
+    function head_y($action) {
+        MVC::body("ware." . substr($action, 2));
+        return parent::head_y($action);
+    }
+
+    function a_profile() { # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
         return ['list' => $this->x_user->all(['is_grp=' => 0])];
     }
 
@@ -12,5 +17,17 @@ class c_acl extends Controller
 
     function a_dpid($id) {
         $this->x_user->dpid($id);
+    }
+
+    function a_grp_user() { # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        return ['list' => $this->x_user->all(['is_grp=' => 1])];
+    }
+
+    function a_cgu($id, $post) {
+        return ['form' => $this->x_user->grp_user($post, $id)];
+    }
+
+    function a_dgu($id) {
+        $this->x_user->dgu($id);
     }
 }
--- mvc/x_user.php
+++ mvc/x_user.php
@@ -8,7 +8,7 @@ class t_user extends \Model_t
 {
     use common;
 
-    function profile($post, $id = 0) {
+    function profile($post, $id = 0) { # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
         $form = [
             '+name' => ['Name'],
             '+comment' => ['Comment'],
@@ -18,6 +18,7 @@ class t_user extends \Model_t
             return new Form($form, $id ? $this->one(['id=' => $id]) : []);
         (new Form($form))->validate();
         $ary = [
+            'is_grp=' => 0,
             'name' => $post->name,
             'comment' => $post->comment,
             '!dt' => '$now',
@@ -30,5 +31,30 @@ class t_user extends \Model_t
         if ($this->delete(['id=' => $id, 'id>' => 2, 'is_grp=' => 0]))
             sqlf('update $_users set pid=2 where pid=%d', $id);
         jump('acl?profile');
+    }
+
+    function grp_user($post, $id = 0) { # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $form = [
+            '+name' => ['Name'],
+            '+comment' => ['Comment'],
+            ['Submit', 'submit', 'onclick="return sky.f.submit()"'],
+        ];
+        if (!$post)
+            return new Form($form, $id ? $this->one(['id=' => $id]) : []);
+        (new Form($form))->validate();
+        $ary = [
+            'is_grp' => 1,
+            'name' => $post->name,
+            'comment' => $post->comment,
+            '!dt' => '$now',
+        ];
+        $id ? $this->update($ary, ['id=' => $id]) : $this->insert($ary);
+        jump('acl?grp_user');
+    }
+
+    function dgu($id) {
+        if ($this->delete(['id=' => $id, 'id>' => 2, 'is_grp=' => 1]))
+            sqlf('update $_users set pid=2 where pid=%d', $id);
+        jump('acl?grp_user');
     }
 }
